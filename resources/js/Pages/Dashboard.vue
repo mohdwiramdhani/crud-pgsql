3771c70bbbbc9a4569b6df7bba948398cefce3c5
Moh Dwi Ramdhani <mohdwiramdhani@gmail.com>
2024-10-21 23:49:37 +0800

    fix bug

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -15,6 +15,8 @@ import { Head } from '@inertiajs/vue3';
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">AHAYou're logged in!</div>
+                    <div class="p-6 text-gray-900">You're logged in!</div>
+                    <div class="p-6 text-gray-900">AHAYou're logged in!</div>
                 </div>
             </div>
         </div>
