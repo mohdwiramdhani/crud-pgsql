f0030db00ca479bbb77e916c85bd7a426ca66321
Moh Dwi Ramdhani <mohdwiramdhani@gmail.com>
2024-10-24 12:51:32 +0800

    Add name dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -17,7 +17,7 @@ import { Head } from '@inertiajs/vue3';
                     <!-- Card 1 -->
                     <div class="bg-white overflow-hidden shadow-md rounded-lg">
                         <div class="p-6">
-                            <h3 class="font-bold text-lg mb-2">Welcome Back!</h3>
+                            <h3 class="font-bold text-lg mb-2">Selamat Datang, {{ $page.props.auth.user.name }}!</h3>
                             <p class="text-gray-700">You are logged in and ready to start.</p>
                         </div>
                     </div>
