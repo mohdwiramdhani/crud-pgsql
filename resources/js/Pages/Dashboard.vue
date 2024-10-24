efa3c0a29a73544a5c3f88664c33aa0c6848abde
Moh Dwi Ramdhani <mohdwiramdhani@gmail.com>
2024-10-24 13:31:35 +0800

    Update dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -8,7 +8,8 @@ import { Head } from '@inertiajs/vue3';
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard Laa</h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard Baru</h2>
         </template>
 
         <div class="py-12">
