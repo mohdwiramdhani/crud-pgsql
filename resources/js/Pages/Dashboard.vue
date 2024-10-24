decbca919ee946fc36cac4b6be79c6cf66c8b8b0
Moh Dwi Ramdhani <mohdwiramdhani@gmail.com>
2024-10-24 20:48:56 +0800

    fix

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -18,7 +18,7 @@ import { Head } from '@inertiajs/vue3';
                     <div class="bg-white overflow-hidden shadow-md rounded-lg">
                         <div class="p-6">
                             <h3 class="font-bold text-lg mb-2">Selamat Datang, {{ $page.props.auth.user.name }}!</h3>
-                            <p class="text-gray-700">You are logged in and ready to start.</p>
+                            <p class="text-gray-700">Silahkan lakukan.</p>
                         </div>
                     </div>
                     <!-- Card 2 -->
